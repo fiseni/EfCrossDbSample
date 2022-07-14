ce8277f42d8fb665a7076b67a4ced637d2a06557
Fati Iseni <fati.iseni@pozitrongroup.com>
2022-07-14 19:50:16 +0200

    Added sample endpoints.

---
+++ src/EfCrossDbSample/Controllers/CustomerController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCrossDbSample.Controllers;
+
+[ApiController]
+public class CustomerController : ControllerBase
+{
+    private readonly AppDbContext _dbContext;
+
+    public CustomerController(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet("/customers/option1")]
+    public async Task<ActionResult<List<CustomerDto>>> GetCustomersOption1(CancellationToken cancellationToken)
+    {
+        var result = _dbContext.Customers
+            .Select(x => new CustomerDto
+            {
+                Id = x.Id,
+                Name = x.Name,
+                CountryName = x.Country.Name
+            });
+
+        return await result.ToListAsync(cancellationToken);
+    }
+
+    [HttpGet("/customers/option2")]
+    public async Task<ActionResult<List<CustomerDto>>> GetCustomersOption2(CancellationToken cancellationToken)
+    {
+        var result = from customer in _dbContext.Customers
+                      join country in _dbContext.Countries on customer.CountryId equals country.Id
+                      select new CustomerDto
+                      {
+                          Id = customer.Id,
+                          Name = customer.Name,
+                          CountryName = country.Name
+                      };
+
+        return await result.ToListAsync(cancellationToken);
+    }
+}
---
+++ src/EfCrossDbSample/Controllers/CustomerDto.cs
@@ -0,0 +1,8 @@
+﻿namespace EfCrossDbSample.Controllers;
+
+public class CustomerDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public string CountryName { get; set; } = default!;
+}
