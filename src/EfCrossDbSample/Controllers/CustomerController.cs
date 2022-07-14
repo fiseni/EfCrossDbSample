b1ee45c633331ed4a67383e5f2da4ef8a014c7c6
Fati Iseni <fati.iseni@pozitrongroup.com>
2022-07-14 21:29:09 +0200

    Added left join samples.

--- src/EfCrossDbSample/Controllers/CustomerController.cs
+++ src/EfCrossDbSample/Controllers/CustomerController.cs
@@ -16,7 +16,9 @@ public class CustomerController : ControllerBase
     [HttpGet("/customers/option1")]
     public async Task<ActionResult<List<CustomerDto>>> GetCustomersOption1(CancellationToken cancellationToken)
     {
-        var result = _dbContext.Customers
+        // Since the CustomerId is nullable, it will generate LEFT JOIN
+
+        var query = _dbContext.Customers
             .Select(x => new CustomerDto
             {
                 Id = x.Id,
@@ -24,13 +26,15 @@ public class CustomerController : ControllerBase
                 CountryName = x.Country.Name
             });
 
-        return await result.ToListAsync(cancellationToken);
+        return await query.ToListAsync(cancellationToken);
     }
 
     [HttpGet("/customers/option2")]
     public async Task<ActionResult<List<CustomerDto>>> GetCustomersOption2(CancellationToken cancellationToken)
     {
-        var result = from customer in _dbContext.Customers
+        // When query syntax is used the default behavior is INNER JOIN.
+
+        var query = from customer in _dbContext.Customers
                       join country in _dbContext.Countries on customer.CountryId equals country.Id
                       select new CustomerDto
                       {
@@ -39,6 +43,24 @@ public class CustomerController : ControllerBase
                           CountryName = country.Name
                       };
 
-        return await result.ToListAsync(cancellationToken);
+        return await query.ToListAsync(cancellationToken);
+    }
+
+    [HttpGet("/customers/option3")]
+    public async Task<ActionResult<List<CustomerDto>>> GetCustomersOption3(CancellationToken cancellationToken)
+    {
+        // Generating LEFT JOIN with query syntax.
+
+        var query = from customer in _dbContext.Customers
+                     join country in _dbContext.Countries on customer.CountryId equals country.Id into grouping
+                     from country in grouping.DefaultIfEmpty()
+                     select new CustomerDto
+                     {
+                         Id = customer.Id,
+                         Name = customer.Name,
+                         CountryName = country.Name
+                     };
+
+        return await query.ToListAsync(cancellationToken);
     }
 }
--- src/EfCrossDbSample/Data/Customer.cs
+++ src/EfCrossDbSample/Data/Customer.cs
@@ -3,7 +3,7 @@
 public class Customer
 {
     public Guid Id { get; set; }
-    public Guid CountryId { get; set; }
+    public Guid? CountryId { get; set; }
     public string Name { get; set; } = default!;
 
     public Country Country { get; set; } = default!;
